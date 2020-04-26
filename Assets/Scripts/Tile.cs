1d6fa77a07071306f42ff6728e6b27114bd64ec1
Luke Hall <hallmluke@gmail.com>
2020-04-26 18:43:28 -0400

    Initial commit

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameObject visualPrefab;
+    public float movementCost = 1;
+    public Coord coord;
+
+    public Material defaultMaterial;
+    public Material movableMaterial;
+
+    Renderer tileRenderer;
+    
+    Unit unitOnTile;
+    Tile parent;
+    public bool isMovable;
+    bool isSelected;
+
+    void Start() {
+
+        tileRenderer = transform.GetComponent<Renderer>();
+        defaultMaterial = tileRenderer.sharedMaterial;
+        movableMaterial = new Material(tileRenderer.sharedMaterial);
+
+        movableMaterial.color = Color.blue;
+    }
+    void Update() {
+
+        if(isMovable) {
+
+            tileRenderer.material = movableMaterial;
+
+        } else {
+
+            tileRenderer.material = defaultMaterial;
+
+        }
+    }
+    public void OccupyTile(Unit unit) {
+        unitOnTile = unit;
+    }
+
+    public void LeaveTile() {
+        unitOnTile = null;
+    }
+
+    public Unit GetUnitOnTile() {
+        return unitOnTile;
+    }
+}
---
+++ Assets/Scripts/TileCursor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileCursor : MonoBehaviour
+{
+    Map map;
+
+    void Awake() {
+        map = FindObjectOfType<Map>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit)) {
+            if(hit.transform.GetComponent<Tile>() != null) {
+
+                transform.position = new Vector3(hit.transform.position.x, .75f, hit.transform.position.z);
+
+            } else if (hit.transform.GetComponent<Unit>() != null) {
+
+                Tile targetTile = hit.transform.GetComponent<Unit>().currentTile;
+
+                transform.position = new Vector3(targetTile.transform.position.x, .75f, targetTile.transform.position.z);
+            }
+
+        }
+    }
+}
